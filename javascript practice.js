966cba1ae9a3180a55cbe20316cb6454b5231fa3
MOHAMMED ABDULSALAM <70317190+abdulsalamMohammed@users.noreply.github.com>
2022-06-21 12:13:09 +0100

    Add files via upload

--- javascript practice.js
+++ javascript practice.js
@@ -5,6 +5,7 @@
 // var convertToNaira = Exchange Rate
 // EXCHANGE RATE = 550Naira to 1 Dollar
 var rate = 620;
+document.getElementById("rate").innerHTML = (rate);
 function dollarToNaira(){
 const currency1 = document.getElementById("currency1").value;
 var dollar = (rate * currency1);
@@ -51,69 +52,113 @@ function resetBgcolor(){
 
 
 
+
 // project three
+// Days in a month
+// choosing a date with more than one switch case
+
+var days = "";
+function getbirthmonth(){  
+  var month = document.getElementById ("birthMonth").value;
+  switch(+month) {
+      case 1:
+      case 2: days = 28;
+        break;
+      case 3:
+      case 4: days = 30;
+        break;
+      case 5:
+      case 6: days = 30;
+        break;
+      case 7:
+      case 8:
+      case 9:  days = 30;
+        break;
+      case 10:
+      case 11: days = 30;
+        break;
+      case 12: days = 31;
+        break;
+    };
+    document.getElementById ("yourBirthMonthIs").innerHTML = ("There are " + days + " days in this month.");
+  };
+
+
+
+
+    
 // Educationa and Salary earned
 var salary = "";
- function showSalary()
- {
-   var education = document.getElementById("eduQualification").value;
-
- 
-switch (education) {
-    case "no high school diploma":
-        salary = 25636;
-        break;
-    case "high school diploma":
-        salary = 35256;
-        break;
-    case "Associate's degree":
-        salary = 41496;
-        break;
-    case "Bachelor's degree":
-        salary = 59124;
-        break;
-    case "Master's degree":
-        salary = 69732;
-        break;
-    case "Professional degree":
-        salary = 89960;
-        break;
-    case "Doctoral degree":
-        salary = 52325520;
-        break;
-};
+var education = document.getElementById("eduQualification").value;
+ function showSalary(){ 
+    switch (education) {
+        case "noHighSchool":
+            salary = 25636;
+            break;
+        case "highSchool":
+            salary = 35256;
+            break;
+        case "Associate":
+            salary = 41496;
+            break;
+        case "Bachelor":
+            salary = 59124;
+            break;
+        case "Master":
+            salary = 69732;
+            break;
+        case "Professional":
+            salary = 89960;
+            break;
+        case "Doctoral":
+            salary = 52325520;
+            break;
+    };
 
 document.getElementById("salary").innerHTML = ("In 2015, a person with "+education+" earned an average of #"+salary.toLocaleString("en-US")+"/year.");
 };
 
 
 // project four
-   
-function calculate()
-{
+
+
+var operators  = Array.from(document.getElementsByClassName("operator"));
+    operators.map( operator => { operator.addEventListener('click', (e) =>{
+    operate =  (e.target.value)
+    //  return (operator.value)     
+        });
+    });
+
+function calculate(){
     var n1 = parseFloat(document.getElementById('n1').value);
     var n2 = parseFloat(document.getElementById('n2').value);
-    var operate = document.getElementById("operate").value;
-
 
     if (operate === '+')
-        {   
-            document.getElementById('answer').value = n1+n2;
-        }
+    {   
+        document.getElementById('answer').value = n1+n2;
+    }
     if (operate === '-')
-        {   
-            document.getElementById('answer').value = n1-n2;
-        }
+    {   
+        document.getElementById('answer').value = n1-n2;
+    }
     if (operate === '*')
-        {   
-            document.getElementById('answer').value = n1*n2;
-        }
+    {   
+        document.getElementById('answer').value = n1*n2;
+    }
     if (operate === '/')
-        {   
-            document.getElementById('answer').value = n1/n2;
-        }
+    {   
+        document.getElementById('answer').value = n1/n2;
+    }
 
-}; 
+};
+
+
+                    
+       
+                  
+
+
+
 
 // project Five
 // winner and prize
